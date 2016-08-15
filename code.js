3f78e5d7e8c9e0051ba231a4931788b929b90d45
joshbang <JoshBangerter@Joshs-MacBook-Pro.local>
2016-08-15 14:49:12 -0600

    notes

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
-var journal = ["DM13 rocks!! Woot!!", "Second thing!", "Third thing!"]
+var journal = ["DM13 rocks!! Woot!!", "Second thing!", "Third thing!"];
+
+
+/*
+cd foldername
+cd ..
+cd ~
+ls
+mkdir newfileName.text
+touch newfileName.text
+
+git status
+git add fileName
+git commit -m
+git push
+
+
+*/
